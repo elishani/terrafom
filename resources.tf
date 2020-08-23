2e5be0a4573645313a5325840e0b18d59243a84b
ELi Shani <shani_50@012.net.il>
2020-08-23 18:21:33 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "default" {
   key_name = "newkey"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDItlI6fXeZPPrEIhPn0Ays4uOiGE0Qip7UJanf6oCE6ORuJC++XO2hhNsOVro9S2ZcJ7vRVaIcPDXzf2ISBdXIq1TdCGn3A/W3Mksaa32j87iIAXql5b7AUwr0+npEvw+ycEFR7mmKVDrTQo/p5yyeWhhyv4i+Qq9kvzYizxUWreKWwoq1ayYh82Ko0dj/ckSIIcULezIMJZz/qylraqzJixLaBr/ru4CxkAxOBMsxt3E4Opnh9C5Dq2sJmGWfDjhoek/5OtSzG0ZPgaIZqGABT0qooWURqkw/4WwQOdbP8PUvPXWUC5rgNBHpYRRGi5mDvcbT2sfX9ApPh3FLGobR"
+  public_key = ""
 }
 
 
